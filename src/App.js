8764c274a88994e714cabac3dcb983336926d635
Tyler <53495442+nuvallo@users.noreply.github.com>
2021-12-10 18:46:49 -0500

    creating data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React from "react"
+import React, { useState } from "react"
 import Header from "./components/Header.js"
 import FeedbackItem from "./components/FeedbackItem.js"
+import FeedbackData from "./data/feedbackData.js"
 
 function App() {
+  const [feedback, setFeedback] = useState(FeedbackData)
   return (
     <>
       <Header />
