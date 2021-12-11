535326556b19a633013df47f87791643ee5e7e61
Tyler <53495442+nuvallo@users.noreply.github.com>
2021-12-10 21:46:53 -0500

    routes setup, icon setup, and about page created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { useState } from "react"
+import { v4 as uuidv4 } from "uuid"
+import { Route, Routes } from "react-router-dom"
 import Header from "./components/Header.js"
 import FeedbackList from "./components/FeedbackList.js"
 import FeedbackStats from "./components/FeedbackStats.js"
 import FeedbackData from "./data/FeedbackData.js"
 import FeedbackForm from "./components/FeedbackForm.js"
+import AboutPage from "./pages/AboutPage.js"
+import AboutIconLink from "./components/AboutIconLink.js"
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData)
+
+  const addFeedBack = (newFeedback) => {
+    newFeedback.id = uuidv4()
+    setFeedback([newFeedback, ...feedback])
+  }
 
   const deleteFeedback = (id) => {
     if (window.confirm("Are you sure you want to delete?")) {
@@ -22,9 +31,25 @@ function App() {
     <>
       <Header />
       <div className="container">
-        <FeedbackForm />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <>
+                <FeedbackForm handleAdd={addFeedBack} />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList
+                  feedback={feedback}
+                  handleDelete={deleteFeedback}
+                />
+              </>
+            }
+          ></Route>
+
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
+        <AboutIconLink />
       </div>
     </>
   )
